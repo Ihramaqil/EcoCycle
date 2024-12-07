607f40d3cb8230dea733b6341a183713d3a6f65e
CarTmannn <muhammadraihanxz12@gmail.com>
2024-12-07 22:50:13 +0800

    foldering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./page/Home/Dashboard"; // Path yang benar
+import Dashboard from "./page/Home/Dashboard";
 
 const App = () => {
   return (
